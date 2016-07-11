2373f708fa024235e3debf80822fb84f8c0703e0
longzhe <dragonlong1986@126.com>
2016-07-11 09:55:21 +0800

    增加了一个单元测试

---
+++ spring-boot-mvc-demo/src/test/java/com/dragon/study/spring/boot/mvc/test/HelloWorldTest.java
@@ -0,0 +1,36 @@
+package com.dragon.study.spring.boot.mvc.test;
+
+import com.dragon.study.spring.boot.mvc.Application;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by dragon on 16/7/10.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+public class HelloWorldTest {
+
+  RestTemplate template = new TestRestTemplate();
+
+  @Test
+  public void testHelloWorld() throws Exception {
+    ResponseEntity responseEntity = template.getForEntity("http://127.0.0.1:8088/mvc/spring-boot/hello-world", String.class);
+    HttpStatus status = responseEntity.getStatusCode();
+    Assert.assertTrue(status.is2xxSuccessful());
+
+    String body = (String)responseEntity.getBody();
+    Assert.assertEquals(body, "Hello World");
+
+  }
+}
