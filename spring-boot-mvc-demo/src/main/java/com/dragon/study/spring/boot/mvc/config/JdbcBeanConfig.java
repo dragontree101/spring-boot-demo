964f1f7446668d5a34e8e28d534a2f9c5edf324c
longzhe <dragonlong1986@126.com>
2016-07-20 10:33:17 +0800

    删除了一些注视

--- spring-boot-mvc-demo/src/main/java/com/dragon/study/spring/boot/mvc/config/JdbcBeanConfig.java
+++ spring-boot-mvc-demo/src/main/java/com/dragon/study/spring/boot/mvc/config/JdbcBeanConfig.java
@@ -7,16 +7,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by dragon on 16/7/12.
  */
 @Configuration
-//@Import(JdbcTemplateConfiguration.class)
 @ComponentScan(value = "com.dragon.study.spring.boot.jdbc")
 public class JdbcBeanConfig {
-
-//  @Bean
-//  public PersonBasicInfoDao mysqlPassInviteCodeStorage(
-//      NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-//    PersonBasicInfoDao personBasicInfoDao = new PersonBasicInfoDao();
-//    personBasicInfoDao.setNamedParameterJdbcTemplate(namedParameterJdbcTemplate);
-//    return personBasicInfoDao;
-//  }
 
 }
