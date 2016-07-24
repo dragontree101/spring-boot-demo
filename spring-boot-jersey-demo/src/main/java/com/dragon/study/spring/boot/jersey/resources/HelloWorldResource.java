cdc912717b2e45ddf019a7904f9224a02c9ee3cf
longzhe <dragonlong1986@126.com>
2016-07-24 20:44:21 +0800

    增加了post和get方法

--- spring-boot-jersey-demo/src/main/java/com/dragon/study/spring/boot/jersey/resources/HelloWorldResource.java
+++ spring-boot-jersey-demo/src/main/java/com/dragon/study/spring/boot/jersey/resources/HelloWorldResource.java
@@ -4,9 +4,12 @@ import com.dragon.study.spring.boot.jersey.service.IHelloJerseyService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +29,23 @@ public class HelloWorldResource {
   @Path("/hello-jersey")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public String helloJersey() {
+  public String helloJersey(@QueryParam("name") String name) {
     String info = "info";
-    log.debug("hello jersey controller, log level is {}", "debug");
-    log.info("hello jersey controller, log level is {}", info);
-    log.warn("hello jersey controller, log level is {}", WARN);
-    log.error("hello jersey controller, log level is error");
+    log.debug("hello jersey controller, name is {}, log level is {}", name, "debug");
+    log.info("hello jersey controller, name is {}, log level is {}", name, info);
+    log.warn("hello jersey controller, name is {}, log level is {}", name, WARN);
+    log.error("hello jersey controller, name is {}, log level is error", name);
+
+    helloWorldService.helloJersey();
+    return "Hello World";
+  }
+
+  @Path("/hello-jersey")
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  public String helloJerseyPost(@FormParam("name") String name) {
+    String info = "info";
+    log.info("hello jersey post controller, name is {}, log level is {}", name, info);
 
     helloWorldService.helloJersey();
     return "Hello World";
