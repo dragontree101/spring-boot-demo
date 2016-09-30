d4b654b6bfc366839c098fe2d69fcd6dd8caa398
longzhe <dragonlong1986@126.com>
2016-09-30 10:03:41 +0800

    添加logging的配置， 按照每天进行切割日志

---
+++ spring-boot-logging/src/main/java/com/dragon/study/spring/boot/logging/Application.java
@@ -0,0 +1,29 @@
+package com.dragon.study.spring.boot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by dragon on 16/9/30.
+ */
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+  @PostConstruct
+  public void logSomething() {
+    for(int i = 0; i<100; i++) {
+      log.debug("Sample Debug Message");
+      log.info("Sample Info Message");
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args).close();
+  }
+
+}
--- spring-boot-no-mvc-demo/src/main/java/com/dragon/study/spring/boot/nomvc/runner/NoMvcRunner.java
+++ spring-boot-no-mvc-demo/src/main/java/com/dragon/study/spring/boot/nomvc/runner/NoMvcRunner.java
@@ -3,14 +3,17 @@ package com.dragon.study.spring.boot.nomvc.runner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Created by dragon on 16/7/10.
  */
 @Component
+@Slf4j
 public class NoMvcRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("hello world");
+    log.info("hello world");
   }
 }
